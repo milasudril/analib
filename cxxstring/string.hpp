6a660cb7bfdd47103d54aa7cdad80747b3750e42
Torbjörn Rathsman <milasudril@gmail.com>
2018-07-31 19:32:39 +0200

    cxxstring module fully functional

--- cxxstring/string.hpp
+++ cxxstring/string.hpp
@@ -6,50 +6,54 @@
 #define ANALIB_STRING_HPP
 
 #include <memory>
+#include <vector>
+#include <string>
 
 namespace Analib
 	{
 	template<class CharT, class Container>
-	class String : private Container
+	class BasicString : private Container
 		{
 		public:
 			using Container::begin;
 			using Container::end;
-			using Container::capacity;
+			using Container::cbegin;
+			using Container::cend;
 			using Container::size;
-			using Container::reserve;
-			using Container::data;
 
-			explicit String(CharT const* cstr)
+			explicit BasicString(CharT const* cstr)
 				{append(cstr);}
 
 			template<class InputIterator>
-			explicit String(InputIterator begin, InputIterator end)
+			explicit BasicString(InputIterator begin, InputIterator end)
 				{append(begin, end);}
 
-			String& append(const CharT* cstr);
-			String& append(String const& str)
+			BasicString& append(CharT const* cstr);
+
+			BasicString& append(BasicString const& str)
 				{return append(str.begin(), str.end());}
 
 			template<class InputIterator>
-			String& append(InputIterator begin, InputIterator end);
+			BasicString& append(InputIterator begin, InputIterator end);
 
-			String& append(CharT ch)
+			BasicString& append(CharT ch)
 				{
-				Container::push_back(ch);
+				this->push_back(ch);
 				return *this;
 				}
+
+			bool operator==(BasicString const& other) const
+				{return *this == static_cast<Container const&>(other);}
 		};
 
 
 	template<class CharT, class Container>
 	template<class InputIterator>
-	String<CharT, Container>& String<CharT,Container>::append(InputIterator begin, InputIterator end)
+	BasicString<CharT, Container>& BasicString<CharT,Container>::append(InputIterator begin, InputIterator end)
 		{
-		reserve(size() + std::distance(begin, end));
 		while(begin!=end)
 			{
-			emplace_back(std::forward<decltype(*begin)>(*begin));
+			append(*begin);
 			++begin;
 			}
 		return *this;
@@ -57,7 +61,61 @@ namespace Analib
 
 
 	template<class CharT, class Container>
-	std::unique_ptr<char const[]> make_cstr(String<CharT, Container> const& str);
+	std::unique_ptr<char const[]> make_cstr(BasicString<CharT, Container> const& str)
+		{
+		auto ret = std::make_unique<char []>(str.size() + 1);
+		std::copy(str.begin(), str.end(), ret.get());
+		*(ret.get() + str.size()) = static_cast<CharT>(0);
+		return std::move(ret);
+		}
+
+	template<class CharT, class Container>
+	inline bool operator!=(BasicString<CharT, Container> const& a, BasicString<CharT, Container> const& b)
+		{return !(a == b);}
+
+	template<class CharT, class Container>
+	bool operator!=(BasicString<CharT, Container> const& a, CharT const* cstr)
+		{
+		auto ch_in = *cstr;
+		auto pos_a = a.begin();
+		while(ch_in!=static_cast<CharT>(0) && pos_a!=a.end())
+			{
+			if(ch_in != *pos_a)
+				{return true;}
+			++cstr;
+			++pos_a;
+			ch_in=*cstr;
+			}
+		return false;
+		}
+
+	template<class CharT, class Container>
+	bool operator==(BasicString<CharT, Container> const& a, CharT const* cstr)
+		{return !(a != cstr);}
+
+	template<class CharT, class Container>
+	bool operator==(CharT const* cstr, BasicString<CharT, Container> const& a)
+		{return cstr == a;}
+
+	template<class CharT, class Container>
+	bool operator!=(CharT const* cstr, BasicString<CharT, Container> const& a)
+		{return cstr != a;}
+
+	using StringNoSso = BasicString<char, std::vector<char>>;
+	using DefaultString = BasicString<char, std::string>;
+
+	template<class CharT, class Container>
+	BasicString<CharT, Container>& BasicString<CharT, Container>::append(const CharT* cstr)
+		{
+		auto ch_in = *cstr;
+		while(ch_in!=static_cast<CharT>(0))
+			{
+			append(ch_in);
+			++cstr;
+			ch_in=*cstr;
+			}
+		return *this;
+		}
 
 	}
 
--- cxxstring/string.test.cpp
+++ cxxstring/string.test.cpp
@@ -2,7 +2,35 @@
 
 #include "string.hpp"
 
+#include <cassert>
+#include <cstring>
+
+static char const teststr[]="Hello, World";
+
+template<class TestType>
+void stringTest()
+	{
+	TestType stringFromCstr{"Hello, World"};
+
+	static_assert(sizeof(teststr)!=sizeof(void*));
+	TestType stringFromRange{teststr, teststr + sizeof(teststr) - 1};  // Must remove nul char here!
+
+	assert(stringFromRange == stringFromCstr);
+	assert(stringFromRange == "Hello, World");
+	assert("Hello, World" == stringFromRange);
+
+	auto copy = stringFromRange;
+	assert(stringFromRange.append("More text") != copy);
+	assert(stringFromRange != "Hello, World");
+
+	assert(copy.append(stringFromCstr)=="Hello WorldHello, World");
+
+	assert(strcmp(make_cstr(TestType{teststr}).get(), teststr) == 0);
+	}
+
 int main()
 	{
+	stringTest<Analib::DefaultString>();
+	stringTest<Analib::StringNoSso>();
 	return 0;
 	}
