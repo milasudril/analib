f4f7a0135d3bbdd896f91470ed504d4be179d7e7
Torbjörn Rathsman <milasudril@gmail.com>
2018-11-20 19:49:22 +0100

    Add InlineString class

---
+++ inlinestring/inlinestring.hpp
@@ -0,0 +1,85 @@
+//@	{
+//@	"targets":[{"name":"inlinestring.hpp","type":"include"}]
+//@	}
+
+#ifndef ANALIB_INLINESTRING_HPP
+#define ANALIB_INLINESTRING_HPP
+
+#include <array>
+#include <cstddef>
+#include <algorithm>
+#include <string>
+
+namespace Analib
+	{
+	template<class CharT, size_t N>
+	class InlineString
+		{
+		public:
+			template<size_t M>
+			constexpr InlineString(CharT const (&val)[M], std::enable_if_t<(M<N), int> = 0) : m_content{}
+				{
+				for(size_t k = 0; k<M; ++k)
+					{m_content[k] = val[k];}
+				}
+
+			constexpr InlineString(CharT const (&val)[N]) : m_content{}
+				{
+				for(size_t k = 0; k<N; ++k)
+					{m_content[k] = val[k];}
+				}
+
+			constexpr CharT const* begin() const {return m_content.begin();}
+
+			constexpr CharT const* end() const {return m_content.end();}
+
+			constexpr CharT* begin() {return m_content.begin();}
+
+			constexpr CharT* end() {return m_content.end();}
+
+			static constexpr auto capacity() {return N;}
+
+			static constexpr auto alignment() {return N<=sizeof(intptr_t) ? sizeof(intptr_t) : 16;}
+
+			static constexpr auto size() {return N;}
+
+#if __cplusplus >= 201703L
+			/**Returns a std::basic_string_view representing the string.
+			 */
+			auto string_view() const
+				{return std::basic_string_view<CharT>{begin(), N};}
+#endif
+
+		private:
+			alignas(alignment()) std::array<CharT, N> m_content;
+		};
+
+	template<class CharT, size_t N>
+	bool operator<(InlineString<CharT, N> const& a, InlineString<CharT, N> const& b)
+		{return std::lexicographical_compare(a.begin(), a.end(), b.begin(), b.end());}
+
+	template<class CharT, size_t N>
+	bool operator>(InlineString<CharT, N> const& a, InlineString<CharT, N> const& b)
+		{return b < a;}
+
+	template<class CharT, size_t N>
+	bool operator==(InlineString<CharT, N> const& a, InlineString<CharT, N> const& b)
+		{return std::equal(a.begin(), a.end(), b.begin(), b.end());}
+
+	template<class CharT, size_t N>
+	bool operator>=(InlineString<CharT, N> const& a, InlineString<CharT, N> const& b)
+		{return !(a < b);}
+
+	template<class CharT, size_t N>
+	bool operator<=(InlineString<CharT, N> const& a, InlineString<CharT, N> const& b)
+		{return !(a > b);}
+
+	template<class CharT, size_t N>
+	bool operator!=(InlineString<CharT, N> const& a, InlineString<CharT, N> const& b)
+		{return !(a == b);}
+
+
+
+	}
+
+#endif
---
+++ inlinestring/inlinestring.test.cpp
@@ -0,0 +1,36 @@
+//@	{
+//@	"targets":[{"name":"inlinestring.test","type":"application","autorun":1}]
+//@	}
+
+#include "inlinestring.hpp"
+
+#include <cassert>
+
+int main()
+	{
+	Analib::InlineString shortString{"1"};
+	static_assert(alignof(shortString) == sizeof(intptr_t), "Bad alignment");
+	static_assert(alignof(shortString) == shortString.alignment(), "Bad alignment");
+	static_assert(shortString.capacity() == 2, "Bad capacity");
+	static_assert(sizeof(shortString) >= shortString.capacity(), "Inline storage");
+	static_assert(static_cast<void*>(shortString.begin()) == static_cast<void*>(&shortString), "Inline storage");
+	static_assert(shortString.size() == shortString.end() - shortString.begin(), "Bad capacity");
+
+
+	Analib::InlineString longString{"01234567"};
+	static_assert(longString.alignment() == 16, "Bad alignment for large string");
+
+	Analib::InlineString a{"Hello"};
+	Analib::InlineString b{"Foo\0\0"};
+	Analib::InlineString c{"Hello"};
+
+	assert( a > b);
+	assert( b < a);
+	assert( a >= b);
+	assert( b <= a);
+	assert( a!= b);
+	assert( a == c);
+
+	auto copy = a;
+	assert( copy == c);
+	}
