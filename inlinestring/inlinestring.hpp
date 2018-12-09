2fbdcb0165dbae7a424fd06086e242fe290cd470
Torbjörn Rathsman <milasudril@gmail.com>
2018-12-09 11:25:09 +0100

    Nodiscard attribute

--- inlinestring/inlinestring.hpp
+++ inlinestring/inlinestring.hpp
@@ -12,6 +12,11 @@
 
 namespace Analib
 	{
+#if __cplusplus >= 201703L
+	#define INLINESTRING_NODISCARD [[nodiscard]]
+#else
+	#define INLINESTRING_NODISCARD
+#endif
 	template<class CharT, size_t N>
 	class InlineString
 		{
@@ -31,24 +36,24 @@ namespace Analib
 					{m_content[k] = val[k];}
 				}
 
-			constexpr CharT const* begin() const {return m_content.begin();}
+			INLINESTRING_NODISCARD constexpr CharT const* begin() const {return m_content.begin();}
 
-			constexpr CharT const* end() const {return m_content.end();}
+			INLINESTRING_NODISCARD constexpr CharT const* end() const {return m_content.end();}
 
-			constexpr CharT* begin() {return m_content.begin();}
+			INLINESTRING_NODISCARD constexpr CharT* begin() {return m_content.begin();}
 
-			constexpr CharT* end() {return m_content.end();}
+			INLINESTRING_NODISCARD constexpr CharT* end() {return m_content.end();}
 
-			static constexpr auto capacity() {return N;}
+			INLINESTRING_NODISCARD static constexpr auto capacity() {return N;}
 
-			static constexpr auto alignment() {return N<=sizeof(intptr_t) ? sizeof(intptr_t) : 16;}
+			INLINESTRING_NODISCARD static constexpr auto alignment() {return N<=sizeof(intptr_t) ? sizeof(intptr_t) : 16;}
 
-			static constexpr auto size() {return N;}
+			INLINESTRING_NODISCARD static constexpr auto size() {return N;}
 
 #if __cplusplus >= 201703L
 			/**Returns a std::basic_string_view representing the string.
 			 */
-			auto string_view() const
+			INLINESTRING_NODISCARD auto string_view() const
 				{return std::basic_string_view<CharT>{begin(), N};}
 #endif
 
@@ -57,31 +62,28 @@ namespace Analib
 		};
 
 	template<class CharT, size_t N>
-	bool operator<(InlineString<CharT, N> const& a, InlineString<CharT, N> const& b)
+	INLINESTRING_NODISCARD inline bool operator<(InlineString<CharT, N> const& a, InlineString<CharT, N> const& b)
 		{return std::lexicographical_compare(a.begin(), a.end(), b.begin(), b.end());}
 
 	template<class CharT, size_t N>
-	bool operator>(InlineString<CharT, N> const& a, InlineString<CharT, N> const& b)
+	INLINESTRING_NODISCARD inline bool operator>(InlineString<CharT, N> const& a, InlineString<CharT, N> const& b)
 		{return b < a;}
 
 	template<class CharT, size_t N>
-	bool operator==(InlineString<CharT, N> const& a, InlineString<CharT, N> const& b)
+	INLINESTRING_NODISCARD inline bool operator==(InlineString<CharT, N> const& a, InlineString<CharT, N> const& b)
 		{return std::equal(a.begin(), a.end(), b.begin(), b.end());}
 
 	template<class CharT, size_t N>
-	bool operator>=(InlineString<CharT, N> const& a, InlineString<CharT, N> const& b)
+	INLINESTRING_NODISCARD inline bool operator>=(InlineString<CharT, N> const& a, InlineString<CharT, N> const& b)
 		{return !(a < b);}
 
 	template<class CharT, size_t N>
-	bool operator<=(InlineString<CharT, N> const& a, InlineString<CharT, N> const& b)
+	INLINESTRING_NODISCARD inline bool operator<=(InlineString<CharT, N> const& a, InlineString<CharT, N> const& b)
 		{return !(a > b);}
 
 	template<class CharT, size_t N>
-	bool operator!=(InlineString<CharT, N> const& a, InlineString<CharT, N> const& b)
+	INLINESTRING_NODISCARD inline bool operator!=(InlineString<CharT, N> const& a, InlineString<CharT, N> const& b)
 		{return !(a == b);}
-
-
-
 	}
 
 #endif
